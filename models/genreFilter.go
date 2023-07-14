5c85be3747bb8ca15fdcf46d5d2b8b567469e2ab
Himasai Reddy <bhsr2003@gmail.com>
2023-07-14 13:30:45 +0530

    GET by filer enabled

--- models/genreFilter.go
+++ models/genreFilter.go
@@ -2,35 +2,38 @@ package model
 
 import (
 	"context"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func FilterByGenre(coll *mongo.Collection, ctx context.Context, context *gin.Context, genre string) {
+func FilterByGenre(coll *mongo.Collection, ctx context.Context, context *gin.Context, reqGenre string) {
+	cursor, err := coll.Find(ctx, bson.M{})
 
+	if err != nil {
+		panic(err)
+	}
+	defer cursor.Close(ctx)
+
+	cnt := 0
+	for cursor.Next(ctx) {
+		var bookList BookData
+		if err = cursor.Decode(&bookList); err != nil {
+			panic(err)
+		}
+		for _, g := range bookList.Genre {
+			if strings.EqualFold(g, reqGenre) {
+				cnt++
+				context.IndentedJSON(http.StatusOK, bookList)
+				break
+			}
+		}
+	}
+
+	if cnt == 0 {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"Message": "The Requested Genre is not Found"})
+	}
 }
-
-// func checkGenre(coll *mongo.Collection, ctx context.Context, reqGenre string) {
-// 	fmt.Printf("\nHere is the list of available books in %v category:- \n", reqGenre)
-// 	cursor, err := coll.Find(ctx, bson.M{})
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	defer cursor.Close(ctx)
-
-// 	for cursor.Next(ctx) {
-// 		var book bson.M
-// 		if err = cursor.Decode(&book); err != nil {
-// 			panic(err)
-// 		}
-
-// 		if genreRegex, ok := book["genre"].(primitive.Regex); ok {
-// 			if strings.Contains(strings.ToLower(genreRegex.Pattern), strings.ToLower(reqGenre)) {
-// 				// PrintBookDetails(book)
-// 			}
-// 		}
-// 	}
-// }
